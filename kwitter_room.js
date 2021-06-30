e77411afe7a46868e541c7d8f9ef178a662dc016
tunishq <74109308+tunishq@users.noreply.github.com>
2021-06-30 18:23:46 +0100

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,3 @@
-
-//ADD YOUR FIREBASE LINKS HERE
 var firebaseConfig = {
       apiKey: "AIzaSyDQb7_ep0iBtDGWVSKAV9ctMphBp0Phy-g",
       authDomain: "kwitter-dfa4b.firebaseapp.com",
@@ -10,7 +8,6 @@ var firebaseConfig = {
       appId: "1:228931856940:web:a73edf0c0074508710bf89",
       measurementId: "G-ML2MXN5YYC"
     };
-    // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
     
@@ -33,11 +30,9 @@ function addRoom()
 
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      //Start code
       console.log("Room Name - "+Room_names);
       row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div> <hr>";
       document.getElementById("output").innerHTML+=row;
-      //End code
       });});}
 getData();
 
